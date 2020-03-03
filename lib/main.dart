0b5c1d1c028ccf3adfc580c383c9e54729fc5651
Fagbenro Damilare Samuel <fagbenrosamuel@gmail.com>
2020-03-04 00:16:02 +0100

    flutter first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex= 0;
+  void _answerQuestion() {
+    setState(() {
+       _questionIndex= _questionIndex + 1;
+    });
+   
+    print(_questionIndex);
+  }
+
+@override
+  Widget build(BuildContext context) {
+    
+    var questions = [
+    'What\'s your favourite color?',
+    'what\'s your favourite animal?',
+  ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //......
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
